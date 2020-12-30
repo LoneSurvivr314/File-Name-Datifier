535512d1844040eaeeb3af2ddf63aae33c42731c
LoneSurvivr314 <LoneSurvivr314@users.noreply.github.com>
2020-12-29 20:21:55 -0700

    Update Name_From_Date.py

--- Name_From_Date.py
+++ Name_From_Date.py
@@ -1,6 +1,8 @@
 import os, time
 import exifread
-directory = r'C:\Users\Old_02\Desktop\File Renamer Test Files'
+directory = r'C:\Users\Old_02\Desktop\File Renamer Sandbox'
 for file in os.scandir(directory):
-    print(exifread.process_file(open(file.path, 'rb')))
-    
+    if file.path.lower().endswith(('.tiff', 'jpg', 'jpeg' 'webp', 'heic')):
+        #print(str(exifread.process_file(open(file.path, 'rb'), details = False)['Image DateTime']) + ' ' + str(os.path.dirname(file.path)))
+        os.rename(str(file.path), str(os.path.dirname(file.path)) + '\\' + str(exifread.process_file(open(file.path, 'rb'), details = False)['Image DateTime']) + str(os.path.split(file.path)[-1]))
+        print(str(file.path))
