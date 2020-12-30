7361f204129b119e6135e5a265faf5b0571352f5
LoneSurvivr314 <LoneSurvivr314@users.noreply.github.com>
2020-12-30 15:43:10 -0700

    Update Name_From_Date.py

--- Name_From_Date.py
+++ Name_From_Date.py
@@ -5,4 +5,4 @@ for file in os.scandir(directory):
     if file.path.lower().endswith(('.tiff', 'jpg', 'jpeg' 'webp', 'heic')):
         #print(str(exifread.process_file(open(file.path, 'rb'), details = False)['Image DateTime']) + ' ' + str(os.path.dirname(file.path)))
         os.rename(str(file.path), str(os.path.dirname(file.path)) + '\\' + str(exifread.process_file(open(file.path, 'rb'), details = False)['Image DateTime']) + str(os.path.split(file.path)[-1]))
-        print(str(file.path))
+        
