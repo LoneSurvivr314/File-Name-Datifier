0344e27ee8136ee1d81ff8fb68c8084ded7e309e
LoneSurvivr314 <LoneSurvivr314@users.noreply.github.com>
2020-12-31 09:17:01 -0700

    Testing

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 import os
 import exifread
-print(str(exifread.process_file(open(r'C:\Users\Old_02\Desktop\File Renamer Test Files\IMG_0001.HEIC', 'rb'), details = False)['Image DateTime']))
+#print(str(exifread.process_file(open(r'C:\Users\Old_02\Desktop\File Renamer Test Files\IMG_0037.PNG', 'rb'), details = False)['Image DateTime']))
+print(r'C:\Users\Old_02\Desktop\File Renamer Test Files\IMG_0037.PNG'.lower().endswith(('.tiff', 'jpg', 'jpeg' 'webp', 'heic')))
