a2fd71f3dad0886bac85cd44b7f1c69bedef0281
LoneSurvivr314 <LoneSurvivr314@users.noreply.github.com>
2020-12-30 19:08:56 -0700

    Update Name_From_Date.py

--- Name_From_Date.py
+++ Name_From_Date.py
@@ -3,13 +3,14 @@ import exifread
 directory = r'C:\Users\Old_02\Desktop\File Renamer Sandbox'
 for file in os.scandir(directory):
     if file.path.lower().endswith(('.tiff', 'jpg', 'jpeg' 'webp', 'heic')):
-        #print(str(exifread.process_file(open(file.path, 'rb'), details = False)['Image DateTime']) + ' ' + str(os.path.dirname(file.path)))
+
+        print(
+            str(file.path)
+            + ' '
+            + str(os.path.dirname(file.path)) + '\\' + str(exifread.process_file(open(file.path, 'rb'), details = False)['Image DateTime']).replace(':', '-') + str(os.path.split(file.path)[-1])
+            )
+
         os.rename(
             file.path,
             str(os.path.dirname(file.path)) + '\\' + str(exifread.process_file(open(file.path, 'rb'), details = False)['Image DateTime']).replace(':', '-') + str(os.path.split(file.path)[-1])
             )
-        #print(
-        #    str(file.path)
-        #    + ' '
-        #    + str(os.path.dirname(file.path)) + '\\' + str(exifread.process_file(open(file.path, 'rb'), details = False)['Image DateTime']).replace(':', '-') + str(os.path.split(file.path)[-1])
-        #    )
