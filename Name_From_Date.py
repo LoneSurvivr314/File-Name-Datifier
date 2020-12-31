1cb9930d34a73987d1f5076b13387935ef59dcc3
LoneSurvivr314 <LoneSurvivr314@users.noreply.github.com>
2020-12-31 15:54:06 -0700

    Added testing

--- Name_From_Date.py
+++ Name_From_Date.py
@@ -3,14 +3,14 @@ import exifread
 directory = r'C:\Users\Old_02\Desktop\File Renamer Test Files'
 for file in os.scandir(directory):
     if file.path.lower().endswith(('.tiff', 'jpg', 'jpeg' 'webp', 'heic')):
-        #print(
-        #    str(file.path)
-        #    + ' '
-        #    + str(os.path.dirname(file.path))
-        #    + '\\' + str(exifread.process_file(open(file.path, 'rb'), details = False)['Image DateTime']).replace(':', '-')
-        #    + ' + '
-        #    + str(os.path.split(file.path)[-1])
-        #    )
+        print(
+            str(file.path)
+            #+ ' '
+            #+ str(os.path.dirname(file.path))
+            #+ '\\' + str(exifread.process_file(open(file.path, 'rb'), details = False)['Image DateTime']).replace(':', '-')
+            #+ ' + '
+            #+ str(os.path.split(file.path)[-1])
+            )
         os.rename(
             file.path,
             str(os.path.dirname(file.path))
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
 import exifread
 #print(str(exifread.process_file(open(r'C:\Users\Old_02\Desktop\File Renamer Test Files\IMG_0037.PNG', 'rb'), details = False)['Image DateTime']))
-print(r'C:\Users\Old_02\Desktop\File Renamer Test Files\IMG_0037.PNG'.lower().endswith(('.tiff', 'jpg', 'jpeg' 'webp', 'heic')))
+str(exifread.process_file(open(r'C:\Users\Old_02\Desktop\File Renamer Test Files\IMG_0176.JPG'
+, 'rb'), details = False)['Image DateTime'])
